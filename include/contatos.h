8f3f15d19dee026fe51e6d9af1e1f4a06aa8f504
beanosz <edmundogabriel567@gmail.com>
2026-03-12 20:34:30 -0300

    Fix header guard and formatting in contatos.h

--- include/contatos.h
+++ include/contatos.h
@@ -1,19 +1,18 @@
 #ifndef CONTATOS_H
 #define CONTATOS_H
 
-// Estrutura do contato com os 3 campos obrigatórios
 typedef struct {
     char nome[50];
     char telefone[20];
     char email[50];
 } Contato;
 
-// Assinaturas das funcoes usando ponteiros (passagem por referencia)
 void cadastrar(Contato *agenda, int *qtd);
 void listar(Contato *agenda, int qtd);
 void buscar(Contato *agenda, int qtd);
 void editar(Contato *agenda, int qtd);
 void excluir(Contato *agenda, int *qtd);
-int contarGmail(Contato *agenda, int qtd, int i); // Funcao recursiva
+int contarGmail(Contato *agenda, int qtd, int i);
+
 
 #endif
